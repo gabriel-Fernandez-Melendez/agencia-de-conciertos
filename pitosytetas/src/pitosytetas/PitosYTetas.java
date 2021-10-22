d83f97970dc1312100125287bad1dc77d4c25259
usut08 <usut08@A204PC13.desarrolloweb.local>
2021-10-22 16:44:58 +0200

    pitos y tetas para todo el que lo lea

---
+++ pitosytetas/src/pitosytetas/PitosYTetas.java
@@ -0,0 +1,10 @@
+package pitosytetas;
+
+public class PitosYTetas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("peras y manzanas");
+	}
+
+}
